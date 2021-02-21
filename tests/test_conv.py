3c4a623ee7d39a1ef1f3b9baa798080903e2ce01
Feng Shi <shi.feng@cs.ucla.edu>
2021-02-20 22:18:49 -0800

    add test for wsconv1d

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -1,6 +1,10 @@
 import torch
 
-from nfnets import WSConv2d, WSConvTranspose2d
+from nfnets import WSConv2d, WSConvTranspose2d, WSConv1d
+
+def test_wsconv1d(k=5):
+    c = WSConv1d(3, 6, kernel_size=k, padding=int(k / 2 + 1))
+    assert c(torch.randn(1, 3, 100)) is not None, "Conv failed."
 
 def test_wsconv2d():
     c = WSConv2d(3,6,3)
