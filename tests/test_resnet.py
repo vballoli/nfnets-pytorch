d5821b2a81a9e0a70808d6a055471df391f4355f
vballoli <balloli.vb@gmail.com>
2021-04-02 00:19:55 +0200

    Update test

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -7,6 +7,35 @@ def test_resnet():
   model = nf_resnet18(activation='celu')
   model2 = nf_resnet50(activation='celu')
   
-  assert model(torch.randn(1,3,224,224)) is not None
-  assert model2(torch.randn(1,3,224,224)) is not None
+  input = torch.randn(1,3,224,224)
+  
+  out = model(input)
+  out2 = model2(input)
+  
+  assert out is not None
+  assert out2 is not None
+  
+  model = nf_resnet18(activation='gelu')
+  model2 = nf_resnet50(activation='gelu')
+  
+  assert model(input) is not None
+  assert model2(input) is not None
+  
+  model = nf_resnet18(activation='silu')
+  model2 = nf_resnet50(activation='silu')
+  
+  assert model(input) is not None
+  assert model2(input) is not None
+  
+  model = nf_resnet18(activation='softplus')
+  model2 = nf_resnet50(activation='softplus')
+  
+  assert model(input) is not None
+  assert model2(input) is not None
+  
+  model = nf_resnet18(activation='elu')
+  model2 = nf_resnet50(activation='elu')
+  
+  assert model(input) is not None
+  assert model2(input) is not None
   
