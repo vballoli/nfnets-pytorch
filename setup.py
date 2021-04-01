f6340f779431cf99a4931ae7b37dc12c8698bae0
vballoli <balloli.vb@gmail.com>
2021-04-01 02:33:08 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(curdir, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'nfnets-pytorch',
   packages = find_packages(),
-  version = '0.1.2',
+  version = '0.1.3',
   license='MIT',
   description = 'NFNets, PyTorch',
   long_description=long_description,
